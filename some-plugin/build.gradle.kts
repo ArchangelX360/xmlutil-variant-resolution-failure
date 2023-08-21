a63b6d28285032e7a5729feaaff99945f67105d3
Titouan Bion <titouan.bion@jetbrains.com>
2023-08-21 11:38:41 +0200

    Add some clarification of what has been tried

--- some-plugin/build.gradle.kts
+++ some-plugin/build.gradle.kts
@@ -1,23 +1,38 @@
+import org.jetbrains.kotlin.gradle.plugin.KotlinPlatformType
+
 plugins {
-  `kotlin-dsl`
-  `java-gradle-plugin`
+    `kotlin-dsl`
+    `java-gradle-plugin`
+    // Does not work with this either:
+    //   id("org.jetbrains.kotlin.jvm") version "1.8.20"
 }
 
 gradlePlugin {
-  plugins {
-    register("somePlugin") {
-      id = "some-plugin"
-      implementationClass = "somepackage.SomePlugin"
+    plugins {
+        register("somePlugin") {
+            id = "some-plugin"
+            implementationClass = "somepackage.SomePlugin"
+        }
     }
-  }
 }
 
 repositories {
     mavenCentral()
 }
 
-dependencies {
-  val xmlutilVersion = "0.86.1"
-  implementation("io.github.pdvrieze.xmlutil:core-jvm:$xmlutilVersion")
-  implementation("io.github.pdvrieze.xmlutil:serialization-jvm:$xmlutilVersion")
+kotlin {
+    target {
+        sourceSets {
+            main {
+                // this does not work either:
+                //
+                //   attributes.attribute(KotlinPlatformType.attribute, KotlinPlatformType.jvm)
+                dependencies {
+                    val xmlutilVersion = "0.86.1"
+                    implementation("io.github.pdvrieze.xmlutil:core-jvm:$xmlutilVersion")
+                    implementation("io.github.pdvrieze.xmlutil:serialization-jvm:$xmlutilVersion")
+                }
+            }
+        }
+    }
 }
